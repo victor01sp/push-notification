1fb32c6f50f287baf3b7991c624f5d4d088c4db1
victor salas <victor001.sp@gmail.com>
2023-09-25 10:35:25 -0500

    new update

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-import express from 'express' 
+const express   = require('express')
+const cors      = require('cors')
+const push      = require('./server/push')
 
 const app = express()
 
+app.use(cors({ origin : '*' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res)=> { 
     res.send('inicio')
+})
+
+app.get('/key', (req, res)=> { 
+    const key = push.getKey();
+    res.send(key)
 })
 
 app.get('/user', (req, res)=> { 
@@ -17,51 +28,7 @@ app.get('/chat', (req, res)=> {
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT)
-
-// const express = require('express')
-// const cors = require('cors');  
-// // const { getPelicula }      = require('./server/db')
-// const push = require('./server/push')
-
-// const app = express()
-
-// app.use(cors({ origin : '*' }));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.get('/key', (req, res)=> {
-//     const key = push.getKey();
-//     res.send(key)
-// })
-
-// app.post('/subscribe', (req, res)=> {
-//     const suscripcion = req.body
-
-//     push.addSuscription( suscripcion )
-
-//     res.json('subscribe')
-// })
-
-// console.log('mamanis');
-
-// app.post('/push', (req, res)=> {
-
-//     const notification = {
-//         title : req.body.title ,
-//         body  : req.body.body,
-//         user  : req.body.user,
-//         icon  : req.body.icon,
-//         url   : req.body.url,
-//     }
-
-//     push.sendPush( notification )
-//     res.json( notification )
-// })
-
-// const PORT = process.env.PORT || 3000
-
-// app.listen(PORT)
-
+ 
 
 
 
