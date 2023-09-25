66ef66d0fffc5e9fd5db502de217403bde591175
victor salas <victor001.sp@gmail.com>
2023-09-24 23:18:18 -0500

    update 2 files

--- app.js
+++ app.js
@@ -1,46 +1,64 @@
-const express = require('express')
-const cors = require('cors');  
-// const { getPelicula }      = require('./server/db')
-const push = require('./server/push')
+import { Express } from 'express' 
 
-const app = express()
-
-app.use(cors({ origin : '*' }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/key', (req, res)=> {
-    const key = push.getKey();
-    res.send(key)
+app.get('/', (req, res)=> { 
+    res.send('inicio')
 })
 
-app.post('/subscribe', (req, res)=> {
-    const suscripcion = req.body
-
-    push.addSuscription( suscripcion )
-
-    res.json('subscribe')
+app.get('/user', (req, res)=> { 
+    res.send('lista de usuario')
 })
 
-console.log('mamanis');
-
-app.post('/push', (req, res)=> {
-
-    const notification = {
-        title : req.body.title ,
-        body  : req.body.body,
-        user  : req.body.user,
-        icon  : req.body.icon,
-        url   : req.body.url,
-    }
-
-    push.sendPush( notification )
-    res.json( notification )
+app.get('/chat', (req, res)=> { 
+    res.send('lista de chat')
 })
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT)
+
+// const express = require('express')
+// const cors = require('cors');  
+// // const { getPelicula }      = require('./server/db')
+// const push = require('./server/push')
+
+// const app = express()
+
+// app.use(cors({ origin : '*' }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.get('/key', (req, res)=> {
+//     const key = push.getKey();
+//     res.send(key)
+// })
+
+// app.post('/subscribe', (req, res)=> {
+//     const suscripcion = req.body
+
+//     push.addSuscription( suscripcion )
+
+//     res.json('subscribe')
+// })
+
+// console.log('mamanis');
+
+// app.post('/push', (req, res)=> {
+
+//     const notification = {
+//         title : req.body.title ,
+//         body  : req.body.body,
+//         user  : req.body.user,
+//         icon  : req.body.icon,
+//         url   : req.body.url,
+//     }
+
+//     push.sendPush( notification )
+//     res.json( notification )
+// })
+
+// const PORT = process.env.PORT || 3000
+
+// app.listen(PORT)
 
 
 
